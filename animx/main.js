af2c8f071129a3ab2cd2122a6290198cb12081c8
FranzRaven <48370444+FranzRaven@users.noreply.github.com>
2023-10-07 17:41:09 -0300

    center

--- animx/main.js
+++ animx/main.js
@@ -1,18 +1,40 @@
 import { useLoader } from "@react-three/fiber"
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import { Suspense } from "react";
+import { Environment, OrbitControls  } from "@react-three/drei";
+import {Canvas} from '@react-three/fiber';
 
 
 
-export default function Model() {
+export  function Model() {
   const gltf = useLoader(GLTFLoader, '/scene.gltf')
   return (
     <>
     {/* Use scale to control the size of the 3D model */}
-    <primitive object={gltf.scene} scale={1} />
+    <primitive object={gltf.scene} scale={1.5} position={[2, -1.8, 0]} rotation={[0,-0.5,0]} />
     </>
 );
-
 };
+
+export  function Odin() {
+  return (
+    <div className='float-left justify-center items-center h-screen' camera={{ position: [0, 0, 0], fov: 50 }} >
+
+      <Canvas >
+        <OrbitControls />
+        <ambientLight intensity={0.9}  />
+        <spotLight intensity={0.9} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
+        <pointLight position={[10, 10, 10]} />
+        <Suspense fallback={null}>
+        <Model  />
+        <Environment preset="city" />
+        </Suspense>
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
+};
+
 
 export function Hola()  {
   return (
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import  Model from  '../animx/main.js';
+{/*import  Model from  '../animx/main.js';*/}
 import {Canvas} from '@react-three/fiber';
 import { Suspense } from "react";
 import { Environment, OrbitControls  } from "@react-three/drei";
 import Head from 'next/head'
 import {Hola} from '../animx/main.js';
+import {Odin} from '../animx/main.js';
 
 
 export default function Home() {
@@ -17,24 +18,13 @@ export default function Home() {
       </Head>
       <div>
         <main className="flex-col">
-          <div className="h12 text-6xl align-middle text-center mt-4" > 
+          <div className="justify-center h12 text-6xl align-middle text-center mt-4" > 
             <h1>3dW0rld</h1> 
           </div>
 
-          <div className="text-center align-middle h-screen">
-            <Canvas className="h-full " 
-              shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
-              <ambientLight intensity={0.7} />
-              <spotLight intensity={0.9} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
-              <Suspense fallback={null}>
-                <Model /> 
-                <Environment preset="city" />
-              </Suspense>
-              <OrbitControls />
-            </Canvas>
-            <div>
-              <Hola />
-            </div> 
+          <div className=" text-center align-middle h-screen ">   
+
+              <Odin />
           </div>
 
         </main>
